b7134d72fcb1d6b4e9c48bede3bf6b55e06927c9
sarangi-ankit <sarangiankit097@gmail.com>
2021-03-21 23:50:20 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,39 +1,78 @@
-// https://api.textgears.com/spelling?key=YYzAtVCWDnDhRBpg&text=my%20name%20is%20amna%20sigh%20solanki&language=en-GB
-function random(){
-    var input2=document.getElementById("input2").value;
-    document.getElementById("input1").value=input2;
+let dummy
+function textBox(){
+    let file=searchFile.files[0]
+    let reader=new FileReader()
+    reader.readAsText(file)
+    reader.onload=()=>{
+        text.innerHTML=reader.result
+        let input=reader.result
+        let input1=input.replace(" ","+")
+        let invalids;
+        const apiKey="YYzAtVCWDnDhRBpg"
+        let api=`https://api.textgears.com/grammar?key=${apiKey}&text=${input1}&language=en-GB`
+        $.ajax({
+            url:api,
+            method:"GET",
+            success:function(result,status){
+                console.log(result)
+                if (status){
+                    invalids=result.response.errors;
+                    for (let invalid of invalids){
+                        input=input.replace(
+                            invalid.bad,
+                            `<span id="mistake" class="mistake">${invalid.bad}</span>`
+                        );
+                    }
+                    text.innerHTML=input
 
- }
- let bl = ''
- var searchBox=document.getElementById("textBox");
+                }
+                let mistakeSpan=document.querySelectorAll(".mistake");
+                for (let i=0;i<mistakeSpan.length;i++){
+                    $(mistakeSpan[i]).contextmenu((event)=>{
+                        // console.log(mistakeSpan[i])
+                        dummy=mistakeSpan[i]
+                        event.preventDefault();
+                        $("#custom > ul").empty()
+                        for (let j=0;j<invalids[i].better.length;j++){
+                            $("#custom > ul").append(`<li class="reference">${invalids[i].better[j]}</li>`)
+                            
+                        }
+                        $("#custom").css("display","block")
+                        $(".reference").mouseover(
+                            (event)=>{
+                                event.target.style.backgroundColor="green";
+                            }
+                        )
+                        $(".reference").mouseout(
+                            (event)=>{
+                                event.target.style.backgroundColor="";
+                            }
+                        )
+                        $(".reference").click(
+                            (event)=>{
+                                let wrongWord=document.getElementsByClassName("mistake");
+                                console.log(wrongWord)
+                                let correctWords=document.querySelectorAll(".reference");
+                                // console.log(correctWords)
+                                
 
- input2.addEventListener("change",()=>{
-     var files=input2.files;
-     if (files.length==0) return;
-     var file=files[0];
+                            //    for (let z=0;z<wrongWord.length;z++){
+                                    // console.log("ankit")
+                                    // if (wrongWord[z].innerHTML==$(event.target)){
+                                    //     wrongWord[z].innerHTML=event.target.innerHTML;
+                                    //     wrongWord[z].classList.remove("mistake");
+                                    //     $("#custom").css("display","none");
+                                    //     break;
+                                    
+                                    // }
+                            //    }
+                            }
+                        )
 
-     var reader=new FileReader();
-     reader.onload=(e)=>{
-         var file=e.target.result;
-         var lines=file.split(/\r\n|\n/);
-         searchBox.value=lines.join('\n');
-         bl = lines.join('\n');
-     };
-     reader.readAsText(file);
-     fetchApi()
- });
+                    })
+                }
+            }
 
-function fetchApi(){
-  
-
-     fetch("https://api.textgears.com/spelling?key=YYzAtVCWDnDhRBpg&text="+searchBox)
-     .then((response)=>{
-         return response.json();
-     }).then((data)=>{
-         console.log(data)
-     })
-     
-     }
-     fetchApi()
- 
-
+        })
+    }
+}
